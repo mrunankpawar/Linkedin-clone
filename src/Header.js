64f539ec3a9de73c95cf991cb36b805652ae1a77
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2024-03-04 19:49:39 -0800

    created header logo and searchbox

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,23 @@
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import LinkedIn from "./img/linkedin-2.png";
 
-function Header() { 
+function Header() {
   return (
-    <div className='header'>
-        <h2>This is header</h2>
+    <div className="header">
+      <div class="header__left">
+        <img src={LinkedIn} alt="" />
 
-        <div class="header__left">
-            <img src="" alt=""/>
-
-            <div class="header__search">
-                <SearchIcon />
-                <input type="text"/>
-            </div>
-
+        <div class="header__search">
+          <SearchIcon />
+          <input type="text" />
         </div>
+      </div>
 
-        <div class="header__right">
-
-        </div>
-    
-    
+      <div class="header__right"></div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
